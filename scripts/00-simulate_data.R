1cb5169518bf3ba74b0401e9cbad2aeb52e11455
Shaffaan Aamir <shaffaanbin.aamir@mail.utoronto.ca>
2024-01-12 16:31:48 -0500

    Simulating data set of interest, it is missing some columns will add later. Testing is also a todo will be added later

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,49 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates bike theft incident reports in different neighborhoods of Toronto
+# Author: Shaffaan Bin Aamir
+# Date: 12 January 2023 
+# Contact: shaffaanbin.aamir@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: Know where to get the data
 # Any other information needed? [...UPDATE THIS...]
 
+#### Work space setup ####
+# Un comment the code below if tidyverse and janitor are not already installed
+# in the computer. Comment it out again once installation is done
+# Only need to isntall once per computer
+#install.packages("tidyverse")
+#install.packages("janitor")
 
-#### Workspace setup ####
+
+
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulated_data <-
+  tibble(
+    # Use 1 through to 151 to represent each division
+    # Randomly pick an option, with replacement, 151 times
+    "Location" = sample(
+      x = c("Little Italy", "Kensigton", "Downtown", "Bay Street", "Christie"),
+      size = 1000,
+      replace = TRUE
+    ),
+    "Date of Incident" = sample(
+      x = c(2015, 2015, 2017, 2018, 2019, 2020),
+      size = 1000,
+      replace = TRUE
+    ),
+    "Cost of Bike (in dollars $)" = sample(
+      x = seq(200, 2000, by = 1),
+      size = 1000,
+      replace = TRUE
+    )
+  )
+
+simulated_data
+simulated_data_sorted <- simulated_data %>% arrange(`Date of Incident`)
+simulated_data_sorted
 
 
 
