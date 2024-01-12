382117b4adccba88c0baf355ac20b29142a06751
Shaffaan Aamir <shaffaanbin.aamir@mail.utoronto.ca>
2024-01-12 17:30:45 -0500

    Downloading data from opendata toronto and saving it in a raw csv file

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,28 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data on Bicycle thefts in Toronto from opendata
+# Author: Shaffaan bin Aamir
+# Date: 12 January 2024
+# Contact: shaffaanbin.aamir@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: None
 # Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(opendatatoronto)     # Need this to download data
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+# Load Bike Theft Data in Toronto
+raw_bike_theft_data <-
+  list_package_resources("c7d34d9b-23d2-44fe-8b3b-cd82c8b38978") |> 
+  filter(name == "bicycle-thefts - 4326.csv") |> 
+  get_resource()
+raw_bike_theft_data
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_bike_theft_data, "inputs/data/raw_data.csv")
 
          
